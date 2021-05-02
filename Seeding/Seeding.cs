596a2965e0c7817609c917e18ddce1c8f1ac29ee
T Matthew Grimsley <timothy.grimsley@revature.net>
2021-05-02 18:49:49 -0400

    Stable version with Autotagging

--- Seeding/Seeding.cs
+++ Seeding/Seeding.cs
@@ -9,16 +9,17 @@ using Repository;
 using Model;
 using System.Linq;
 using Logic.ApiHelper;
+using System.Globalization;
 
 namespace Seeding
 {
     public class Seeding
     {
-        const int MAX_GET_MOVIE_TASKS = 10;
-        const int MAX_ADD_WORD_TASKS = 10;
-        const int MAX_ADD_MOVIE_TASKS = 10;
-        const int BATCH_SIZE = 15;
-        const int STARTING_LINE = 3400;
+        const int MAX_MOVIE_API_TASKS = 5;
+        const int MAX_WORD_API_TASKS = 5;
+        const int MAX_DB_TASKS = 15;
+        const int BATCH_SIZE = 50;
+        const int STARTING_LINE = 4600;
         const int MAX_NUMBER_OF_MOVIES = 100;
         readonly static DbContextOptions<Cinephiliacs_MovieContext> dbOptions = new DbContextOptionsBuilder<Cinephiliacs_MovieContext>()
                 .UseSqlServer("Server=tcp:cinephiliacs.database.windows.net,1433;Initial Catalog=Cinephiliacs_Movie;"
@@ -53,7 +54,7 @@ namespace Seeding
             int limitCounter = 0;
             var getMovieTasks = new List<Task<MovieDTO>>();
             var movieDTOs = new List<MovieDTO>();
-            var addMovieBatchTasks = new List<Task>();
+            var addMovieBatchTasks = new List<Task<bool>>();
             
             for (int index = STARTING_LINE; index < movieIdList.Count
                 && limitCounter <= MAX_NUMBER_OF_MOVIES; index++)
@@ -61,7 +62,7 @@ namespace Seeding
                 getMovieTasks.Add(GetMovie(movieIdList[index]));
 
                 limitCounter++;
-                while(getMovieTasks.Count >= MAX_GET_MOVIE_TASKS)
+                while(getMovieTasks.Count >= MAX_MOVIE_API_TASKS)
                 {
                     var completedTask = await Task<MovieDTO>.WhenAny(getMovieTasks);
                     if(completedTask.Result != null)
@@ -89,6 +90,7 @@ namespace Seeding
                     while (addMovieBatchTasks.Count > 0)
                     {
                         var completedTask = await Task<List<MovieDTO>>.WhenAny(addMovieBatchTasks);
+                        Console.WriteLine(completedTask.Result.ToString());
                         addMovieBatchTasks.Remove(completedTask);
                     }
                     Console.WriteLine("Starting Batch with " + movieDTObatch.Count.ToString() + " Movies to Add!");
@@ -132,6 +134,7 @@ namespace Seeding
                 while (addMovieBatchTasks.Count > 0)
                 {
                     var completedTask = await Task<List<MovieDTO>>.WhenAny(addMovieBatchTasks);
+                    Console.WriteLine(completedTask.Result.ToString());
                     addMovieBatchTasks.Remove(completedTask);
                 }
                 Console.WriteLine("Starting Batch with " + movieDTObatch.Count.ToString() + " Movies to Add!");
@@ -143,12 +146,19 @@ namespace Seeding
             while (addMovieBatchTasks.Count > 0)
             {
                 var completedTask = await Task<List<MovieDTO>>.WhenAny(addMovieBatchTasks);
+                Console.WriteLine(completedTask.Result.ToString());
                 addMovieBatchTasks.Remove(completedTask);
             }
   
             return true;
         }
         
+        /// <summary>
+        /// Gets a Movie from the public API. Splits the plot into individual words,
+        /// removes duplicate words (case-insensitive), save them in the MovieTags list.
+        /// </summary>
+        /// <param name="movieId"></param>
+        /// <returns></returns>
         public static async Task<MovieDTO> GetMovie(string movieId)
         {
             Logic.ApiHelper.MovieObject movieObject = await Logic.ApiHelper.ApiProcessor
@@ -159,6 +169,17 @@ namespace Seeding
             }
             MovieDTO newMovieDTO = Mapper.MovieObjectToMovieDTO(movieObject);
             newMovieDTO.MovieTags = SplitPlotIntoWords(newMovieDTO.Plot);
+            for (int i = newMovieDTO.MovieTags.Count - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if(newMovieDTO.MovieTags[j].ToLower() == newMovieDTO.MovieTags[i].ToLower())
+                    {
+                        newMovieDTO.MovieTags.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
 
             return newMovieDTO;
         }
@@ -193,8 +214,9 @@ namespace Seeding
         /// </summary>
         /// <param name="movieDTOs"></param>
         /// <returns></returns>
-        public static async Task AddMovies(List<MovieDTO> movieDTOs)
+        public static async Task<bool> AddMovies(List<MovieDTO> movieDTOs)
         {
+            Console.WriteLine("Accumulating Tags");
             // Use temporary object lists to accumulate all of the
             // Actors, Directors, Genres, Languages and Ratings that exist
             // in the movies to be added. These need to be added ahead of
@@ -231,6 +253,7 @@ namespace Seeding
                 }
             }
 
+            Console.WriteLine("Removing Duplicates");
             // Remove duplicate entries, case-insensitive
             for (int i = actors.Count - 1; i >= 0; i--)
             {
@@ -291,7 +314,7 @@ namespace Seeding
             {
                 for (int j = 0; j < i; j++)
                 {
-                    if(plotWords[j].ToLower() == plotWords[i].ToLower())
+                    if(plotWords[j] == plotWords[i])
                     {
                         plotWords.RemoveAt(i);
                         break;
@@ -299,14 +322,15 @@ namespace Seeding
                 }
             }
 
+            Console.WriteLine("Processing Words");
             // Process each word, Adding any to the database that don't already exist.
             // Also add any new Tags.
             var addWordTasks = new List<Task>();
             foreach (var word in plotWords)
             {
-                addWordTasks.Add(AddWord(word));
+                addWordTasks.Add(Task.Run(() => AddWord(word)));
 
-                while(addWordTasks.Count >= MAX_ADD_WORD_TASKS)
+                while(addWordTasks.Count >= MAX_WORD_API_TASKS)
                 {
                     var completedTask = await Task.WhenAny(addWordTasks);
                     addWordTasks.Remove(completedTask);
@@ -319,6 +343,8 @@ namespace Seeding
                 addWordTasks.Remove(completedTask);
             }
 
+
+            Console.WriteLine("Add Prereqs. to Db");
             // Add new Actor, Director, Genre, Language, Rating Objects to the database
             using (var context = new Cinephiliacs_MovieContext(dbOptions))
             {
@@ -365,13 +391,15 @@ namespace Seeding
                 context.SaveChanges();
             }
 
+
+            Console.WriteLine("Adding Movies");
             // Process each Movie.
             var addMovieTaskList = new List<Task<string>>();
             foreach (var movieDTO in movieDTOs)
             {
-                addMovieTaskList.Add(AddMovie(movieDTO));
+                addMovieTaskList.Add(Task.Run(() => AddMovie(movieDTO)));
 
-                while(addMovieTaskList.Count >= MAX_ADD_MOVIE_TASKS)
+                while(addMovieTaskList.Count >= MAX_DB_TASKS)
                 {
                     var completedTask = await Task.WhenAny(addMovieTaskList);
                     if(completedTask == null)
@@ -399,6 +427,7 @@ namespace Seeding
                 }
                 addMovieTaskList.Remove(completedTask);
             }
+            return true;
         }
 
         /// <summary>
@@ -413,32 +442,26 @@ namespace Seeding
         {
             using (var context = new Cinephiliacs_MovieContext(dbOptions))
             {
+                bool wordIsTag;
+
                 Word dbWord = context.Words.FirstOrDefault(w => w.Word1 == word);
                 if(dbWord == null)
                 {
                     var wordObject = await Logic.ApiHelper.ApiProcessor.LoadDefinitionAsync(word);
 
-                    bool wordIsTag = ProcessWordObject(word, wordObject, context);
-
-                    if(wordIsTag)
-                    {
-                        Tag tag = new Tag();
-                        tag.TagName = word;
-                        tag.IsBanned = false;
-                        context.Tags.Add(tag);
-                    }
+                    wordIsTag = ProcessWordObject(word, wordObject, context);
                 }
                 else
                 {
-                    if(dbWord.IsTag)
+                    if(context.Tags.FirstOrDefault(t => t.TagName == dbWord.BaseWord) == null)
                     {
                         Tag tag = new Tag();
-                        tag.TagName = word;
+                        tag.TagName = dbWord.BaseWord;
                         tag.IsBanned = false;
                         context.Tags.Add(tag);
                     }
                 }
-                
+
                 context.SaveChanges();
             }
             return;
@@ -464,6 +487,17 @@ namespace Seeding
             newWord.IsTag = wordIsTag;
             newWord.Word1 = originalWord;
             newWord.BaseWord = wordObject.Word;
+
+            if(wordIsTag)
+            {
+                if(context.Tags.FirstOrDefault(t => t.TagName == originalWord) == null)
+                {
+                    Tag tag = new Tag();
+                    tag.TagName = originalWord;
+                    tag.IsBanned = false;
+                    context.Tags.Add(tag);
+                }
+            }
             
             if(context.Words.FirstOrDefault(w => w.Word1 == originalWord) == null)
             {
@@ -476,6 +510,19 @@ namespace Seeding
                 baseWord.IsTag = wordIsTag;
                 baseWord.Word1 = wordObject.Word;
                 baseWord.BaseWord = wordObject.Word;
+                
+                if(wordIsTag)
+                {
+                    if(context.Tags.FirstOrDefault(t => t.TagName == wordObject.Word) == null)
+                    {
+                        Tag tag = new Tag();
+                        tag.TagName = wordObject.Word;
+                        tag.IsBanned = false;
+                        context.Tags.Add(tag);
+                    }
+                }
+
+                context.SaveChanges();
 
                 if(context.Words.FirstOrDefault(w => w.Word1 == wordObject.Word) == null)
                 {
@@ -518,22 +565,182 @@ namespace Seeding
                 RepoLogic _repo = new RepoLogic(context);
                 IMovieLogic _logic = new MovieLogic(_repo);
 
-                if(movieDTO != null)
+                if(await CreateMovie(movieDTO, context))
                 {
-                    if(await _logic.CreateMovie(movieDTO))
-                    {
-                        return movieDTO.ImdbId + ": Success!";
-                    }
-                    else
-                    { 
-                        return movieDTO.ImdbId + ": Movie already exists!";
-                    }
+                    return movieDTO.ImdbId + ": Success!";
                 }
                 else
-                {
-                    return movieDTO.ImdbId + ": Movie not found!";
+                { 
+                    return movieDTO.ImdbId + ": Movie already exists!";
                 }
             }
+        }
+        
+        public static async Task<bool> CreateMovie(MovieDTO movieDTO, Cinephiliacs_MovieContext context)
+        {
+            if(movieDTO.ImdbId == null || context.Movies.FirstOrDefault(m => m.ImdbId == movieDTO.ImdbId) != null)
+            {
+                return false;
+            }
+
+            Movie movie = new Movie();
+            movie.ImdbId = movieDTO.ImdbId;
+
+            if(!String.IsNullOrEmpty(movieDTO.Title))
+            {
+                movie.Title = movieDTO.Title;
+            }
+
+            if(!String.IsNullOrEmpty(movieDTO.RatingName))
+            {
+                Rating rating = context.Ratings.FirstOrDefault(r => r.RatingName == movieDTO.RatingName);
+                if(rating != null)
+                {
+                    movie.RatingId = rating.RatingId;
+                }
+            }
+
+            if(!String.IsNullOrEmpty(movieDTO.ReleaseDate))
+            {
+                movie.ReleaseDate = DateTime.ParseExact(movieDTO.ReleaseDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                movie.IsReleased = true;
+            }
+            if(!String.IsNullOrEmpty(movieDTO.ReleaseCountry))
+            {
+                movie.ReleaseCountry = movieDTO.ReleaseCountry;
+            }
+            if(movieDTO.RuntimeMinutes != null)
+            {
+                movie.RuntimeMinutes = movieDTO.RuntimeMinutes;
+            }
+            if(!String.IsNullOrEmpty(movieDTO.Plot))
+            {
+                movie.Plot = movieDTO.Plot;
+            }
+            if(!String.IsNullOrEmpty(movieDTO.PosterURL))
+            {
+                movie.PosterUrl = movieDTO.PosterURL;
+            }
+
+            context.Movies.Add(movie);
+            context.SaveChanges();
+
+            int counter = 0;
+            var processWordTaskList = new List<Task<bool>>();
+            foreach (var plotWord in movieDTO.MovieTags)
+            {
+                processWordTaskList.Add(ProcessWordInDb(plotWord, movieDTO.ImdbId, context));
+
+                while(processWordTaskList.Count >= MAX_DB_TASKS)
+                {
+                    var completedTask = await Task.WhenAny(processWordTaskList);
+                    if(completedTask.Result)
+                    {
+                        counter++;
+                    }
+                    processWordTaskList.Remove(completedTask);
+                }
+            }
+            // Await final ProcessWordInDb tasks
+            while(processWordTaskList.Count > 0)
+            {
+                var completedTask = await Task.WhenAny(processWordTaskList);
+                if(completedTask.Result)
+                {
+                    counter++;
+                }
+                processWordTaskList.Remove(completedTask);
+            }
+
+            if(movieDTO.MovieActors != null)
+            {
+                foreach (var movieActorName in movieDTO.MovieActors)
+                {
+                    Actor actor = context.Actors.FirstOrDefault(l => l.ActorName == movieActorName);
+                    
+                    var movieActor = new MovieActor();
+                    movieActor.ImdbId = movieDTO.ImdbId;
+                    movieActor.ActorId = actor.ActorId;
+                    context.MovieActors.Add(movieActor);     
+                }
+            }
+            if(movieDTO.MovieDirectors != null)
+            {
+                foreach (var movieDirectorName in movieDTO.MovieDirectors)
+                {
+                    Director director = context.Directors.FirstOrDefault(l => l.DirectorName == movieDirectorName);
+                    
+                    var movieDirector = new MovieDirector();
+                    movieDirector.ImdbId = movieDTO.ImdbId;
+                    movieDirector.DirectorId = director.DirectorId;
+                    context.MovieDirectors.Add(movieDirector);     
+                }
+            }
+            if(movieDTO.MovieGenres != null)
+            {
+                foreach (var movieGenreName in movieDTO.MovieGenres)
+                {
+                    Genre genre = context.Genres.FirstOrDefault(g => g.GenreName == movieGenreName);
+                    
+                    var movieGenre = new MovieGenre();
+                    movieGenre.ImdbId = movieDTO.ImdbId;
+                    movieGenre.GenreId = genre.GenreId;
+                    context.MovieGenres.Add(movieGenre);     
+                }
+            }
+            if(movieDTO.MovieLanguages != null)
+            {
+                foreach (var movieLanguageName in movieDTO.MovieLanguages)
+                {
+                    Language language = context.Languages.FirstOrDefault(l => l.LanguageName == movieLanguageName);
+                    
+                    MovieLanguage movieLanguage = new MovieLanguage();
+                    movieLanguage.ImdbId = movieDTO.ImdbId;
+                    movieLanguage.LanguageId = language.LanguageId;
+                    context.MovieLanguages.Add(movieLanguage);                    
+                }
+            }
+            context.SaveChanges();
+            return true;
+        }
+
+        private static async Task<bool> ProcessWordInDb(string word, string movieId, Cinephiliacs_MovieContext context)
+        {
+            Word dbWord = context.Words.FirstOrDefault(w => w.Word1 == word);
+            if(dbWord != null)
+            {
+                if(dbWord.IsTag)
+                {
+                    var movieTagUser = new MovieTagUser();
+                    movieTagUser.ImdbId = movieId;
+                    movieTagUser.UserId = "~AutoGenerated";
+                    movieTagUser.TagName = dbWord.BaseWord;
+                    movieTagUser.IsUpvote = true;
+                    
+                    if(context.MovieTagUsers.FirstOrDefault(mtu => mtu.ImdbId == movieTagUser.ImdbId
+                        && mtu.TagName == movieTagUser.TagName && mtu.UserId == movieTagUser.UserId) == null)
+                    {
+                        var movieTag = context.MovieTags.FirstOrDefault(mt => mt.ImdbId == movieTagUser.ImdbId
+                            && mt.TagName == movieTagUser.TagName);
+                        
+                        if(movieTag == null)
+                        {
+                            movieTag = new MovieTag();
+                            movieTag.ImdbId = movieTagUser.ImdbId;
+                            movieTag.TagName = movieTagUser.TagName;
+                            movieTag.VoteSum = 0;
+                            context.MovieTags.Add(movieTag);
+                        }
+
+                        if(movieTag.VoteSum < int.MaxValue)
+                            movieTag.VoteSum += 1;
+
+                        context.MovieTagUsers.Add(movieTagUser);
+                        context.SaveChanges();
+                    }
+                }
+            }
+            return true;
         }
     }
 }
