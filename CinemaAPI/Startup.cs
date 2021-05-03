d952d69801f0356aa512314e98cc4d7991798b6f
Christopher Larson <57510138+clarson217@users.noreply.github.com>
2021-05-03 18:55:32 -0400

    Update Startup.cs

--- CinemaAPI/Startup.cs
+++ CinemaAPI/Startup.cs
@@ -41,6 +41,8 @@ namespace CinemaAPI
                             "http://20.94.137.143", //Frontend
                             "http://20.189.29.112", //Admintools
                             "http://20.45.2.119", //User
+                            "http://20.45.6.142", //Forum
+                            "http://20.189.30.176", //Review
                             "http://localhost:4200" // for testing
                             )
                             .AllowAnyHeader()
