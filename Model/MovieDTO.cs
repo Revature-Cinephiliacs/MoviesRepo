496adb60fafbc2419d3a3c70ede14ea035fcc831
T Matthew Grimsley <timothy.grimsley@revature.net>
2021-05-04 14:49:36 -0400

    Added xUnit tests

--- Model/MovieDTO.cs
+++ Model/MovieDTO.cs
@@ -8,7 +8,7 @@ namespace Model
     /// Contains the movie information that is sent from and returned
     /// to the frontend.
     /// </summary>
-    public sealed class MovieDTO : IEquatable<MovieDTO>
+    public sealed class MovieDTO
     {
         [StringLength(20)]
         public string ImdbId { get; set; }
@@ -37,55 +37,6 @@ namespace Model
         public List<string> MovieTags { get; set; }
 
         public MovieDTO() {}
-
-        public bool Equals(MovieDTO other)
-        {
-            if (Object.ReferenceEquals(other, null))
-            {
-                return false;
-            }
-
-            if (Object.ReferenceEquals(this, other))
-            {
-                return true;
-            }
-
-            if (this.GetType() != other.GetType())
-            {
-                return false;
-            }
-
-            return ImdbId == other.ImdbId;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return this.Equals(obj as MovieDTO);
-        }
-
-        public static bool operator ==(MovieDTO lhs, MovieDTO rhs)
-        {
-            if (Object.ReferenceEquals(lhs, null))
-            {
-                if (Object.ReferenceEquals(rhs, null))
-                {
-                    return true;
-                }
-
-                return false;
-            }
-            return lhs.Equals(rhs);
-        }
-
-        public static bool operator !=(MovieDTO lhs, MovieDTO rhs)
-        {
-            return !(lhs == rhs);
-        }
-
-        public override int GetHashCode()
-        {
-            return ImdbId.GetHashCode();
-        }
         
     }
 }
